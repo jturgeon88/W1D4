11d03f8d36ce02a6238d952a66bda1376ff84a41
AA Student <student@appacademy.io>
2017-10-05 15:18:56 -0700

    Fixed bug in board.rb deleted extra end

--- board.rb
+++ board.rb
@@ -55,12 +55,13 @@ class Board
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
-    end
+
   end
 
   def solved_set?(tiles)
     nums = tiles.map(&:value)
     nums.sort == (1..9).to_a
+
   end
 
   def square(idx)
